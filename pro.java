2e6036d97a8ff30ce3251bc1cb6bb76afd24de42
Maya Cajina <maya.cajina123@std.uni.edu.ni>
2025-06-23 15:50:03 -0600

    misegundo cambio confirmado

--- pro.java
+++ pro.java
@@ -1,8 +1,8 @@
 public class pro {
 public static void main(String[] args) {
 String nombre= "Estudiante";
-int a = 5;
-int b = 3;
+int a =10;
+int b = 0;
 int suma = a+b;
 System.out.println("Hola," + nombre + "!");
 System.out.println("La suma de"+ a +"+"+ b +"es:"+ suma);
